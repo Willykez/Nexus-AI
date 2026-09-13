7846a804e2fdf104b9f548bbe9df9afc43e49c9e
Repo Master <repomaster@local>
2026-09-13 06:30:03 +0300

    Update 3 files in app/src/main/java/com/nexusforge/app/ui

--- app/src/main/java/com/nexusforge/app/ui/components/ProviderFormDialog.kt
+++ app/src/main/java/com/nexusforge/app/ui/components/ProviderFormDialog.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import com.nexusforge.app.data.ProviderProfile
 import com.nexusforge.app.data.SettingsStore
 
